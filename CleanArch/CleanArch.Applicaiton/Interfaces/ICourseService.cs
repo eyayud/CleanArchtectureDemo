146e783d7e087ec0464bc7034e7c39415743894d
eyayud <eyayu.demisse@gmail.com>
2021-09-02 11:54:24 +0300

    commit service and repository  interfaces  and courseview model

---
+++ CleanArch/CleanArch.Applicaiton/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArch.Applicaiton.ViewModels;
+using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Applicaiton.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArch/CleanArch.Applicaiton/ViewModels/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Applicaiton.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<CourseViewModel> Courses { get; set; }
+    }
+}
